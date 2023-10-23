fe801ee7531daf89d7493f8d43d1b4b14fd8fdbd
HOSSAM-ELDIN <HOS-ELDIN@users.noreply.github.com>
2023-10-23 08:25:59 +0300

    Add language selection

--- main.js
+++ main.js
@@ -1,28 +1,89 @@
 let icons = document.querySelectorAll(".score img");
 let category = document.querySelectorAll(".score img + span");
 let resultScore = document.querySelectorAll(".result-score");
-let finalScore = document.querySelector(".final")
+let finalScore = document.querySelector(".final");
 let scoresArray = [];
 
+// get language selector from page
+let LanguageSelector = document.getElementById("lang");
 
-fetch("data.json")
-	.then((response) => {
-		let data = response.json();
-		return data;
-	})
-	.then((data) => {
+// get the element to change text
+let mainHead = document.getElementById("main-head");
+let great = document.querySelector(".great");
+let resultText = document.querySelector("#result-text");
 
-		//this is so much easier
-		data.forEach((e, i) => {
-			icons[i].src = e.icon;
-			category[i].innerHTML = e.category;
-			resultScore[i].innerHTML = e.score;
-      scoresArray.push(e.score)
-		});
+// get the body to change dir
+let page = document.body;
 
-    let average = scoresArray.reduce((x,y)=> x + y)/scoresArray.length
-    finalScore.innerHTML=average.toFixed()
+// declare the language variable
+let lang;
 
-	});
+// set the language on page load
+window.onload = (e) => {
+  lang = LanguageSelector.value;
+  fetchPage(lang);
+  setPageDir(lang);
+};
 
-  // done el7
+// add event listener to get the chosen language
+LanguageSelector.addEventListener("change", (e) => {
+  lang = e.target.value;
+  fetchPage(lang);
+  setPageDir(lang);
+});
+
+// function to get the right data upon the selected language
+const fetchPage = (lang) => {
+  fetch(`languages/${lang}.json`)
+    .then((response) => {
+      let data = response.json();
+      return data;
+    })
+    .then((data) => {
+      // set the translation to the elements
+      mainHead.innerHTML = data.head;
+      great.innerHTML = data.great;
+      resultText.innerHTML = data.text;
+
+      // set the results
+      //this is so much easier
+      data.result.forEach((e, i) => {
+        icons[i].src = e.icon;
+        category[i].innerHTML = e.category;
+        resultScore[i].innerHTML = e.score;
+        scoresArray.push(e.score);
+      });
+
+      // calculating the langauge
+      let average = scoresArray.reduce((x, y) => x + y) / scoresArray.length;
+      finalScore.innerHTML = average.toFixed();
+    });
+};
+
+// function to set the page dir upon the selected language
+const setPageDir = (lang) => {
+  lang === "ar"
+    ? page.setAttribute("dir", "rtl")
+    : page.setAttribute("dir", "ltr");
+};
+
+
+// fetch("data.json")
+//   .then((response) => {
+//     let data = response.json();
+//     return data;
+//   })
+//   .then((data) => {
+//     //this is so much easier
+//     data.forEach((e, i) => {
+//       icons[i].src = e.icon;
+//       category[i].innerHTML = e.category;
+//       resultScore[i].innerHTML = e.score;
+//       scoresArray.push(e.score);
+//     });
+
+//     let average = scoresArray.reduce((x, y) => x + y) / scoresArray.length;
+//     finalScore.innerHTML = average.toFixed();
+//   });
+
+// done el7
